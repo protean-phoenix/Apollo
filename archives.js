c7589fb86bf9ea400209851211f8913045d6701e
Claudius King <tezcatlipoca7123@gmail.com>
2021-12-02 17:20:21 -0800

    message 12/2/21

--- archives.js
+++ archives.js
@@ -1,3 +1,9 @@
-const archives = [{'title':'One', 'text':['text one','text one two']},
-  {'title':'Two', 'text':['text two one', 'text two two']}
+const archives = [{'title':'Madonna', 'text':['embodiement of conquest\nlike coming home after a long quest\nrelief from the pestilence\nleft only beauty and excellence',
+'rocking scarlet on the wrist\nsomeone knows an eye can\'t resist\n"never forget" ringing through a skull\nremember, the sun doesn\'t grow dull',
+'dressed in black to remind\nliberty to speak what\'s on the mind\nlibra with eyes closed like nemesis\nseeing animals fresh like eve from genesis',
+'pale in her head...\nleaves death dead\nlike someones got the ear to hear\nand wipe the slate clean of fear']},
+  {'title':'With the Moon', 'text':
+  ['There\'s a woman clothed with the sun\nAtmosphere like Io\'s come to Saturn\'s seat\nSending to the crown sounds like fun\nReflection of a worm lies beneath her feet\n',
+'Sword in hand like someone\'s come to take the piece\nPicture of three in pocket and that\'ll never cease\nI\'d be lying if I said a kid didn\'t feel some scarlet\nZodiac in your head\'s got a convo star lit\n',
+'In space everywhere you go the vacuum will follow\nEscape it in the forest for many moons\nElse the serpent that covers will leave you hollow\nCan\'t wait to hear you birth some tunes']}
 ];
--- message.js
+++ message.js
@@ -1,3 +1,1 @@
-const message = [{'title':'Three', 'text':['text one','text one two']},
-  {'title':'Four', 'text':['text two one', 'text two two']}
-];
+const message = [archives[0], archives[1]];
